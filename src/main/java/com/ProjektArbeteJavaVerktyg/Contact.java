a7ee70219e91ba218e1a1a158ba45905be08aaaa
[Muharem <[Muharem.z10@hotmail.com]>
2020-11-26 22:50:40 +0100

    first commit

---
+++ src/main/java/com/ProjektArbeteJavaVerktyg/Contact.java
@@ -0,0 +1,27 @@
+package com.ProjektArbeteJavaVerktyg;
+
+public class Contact {
+
+    private String FullName;
+    private String Email;
+    private String PhoneNumber;
+    private String Address;
+
+
+    public Contact(String Fullname, String Email, String PhoneNumber, String Address) {
+
+        this.FullName;
+        this.Email;
+        this.PhoneNumber;
+        this.Address;
+    }
+    public String getFullName(){return FullName;}
+
+    public String getEmail(){return Email;}
+
+    public String getPhoneNumber(){return PhoneNumber;}
+
+    public String getAddress(){return Address;}
+
+
+}
